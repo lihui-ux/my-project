ffa16cce2eb04d983837a2de33c995423b1c5f24
lihui <1564209260@qq.com>
2020-08-11 20:29:27 +0800

    初始化项目，搭建路由组件

---
+++ src/pages/Search/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Search<br>
+    params参数：{{$route.params.keyword}}<br>
+    query参数：{{$route.query.keyword}}<br>
+    props--params参数：{{keyword1}}<br>
+     props--query：{{keyword2}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props:["keyword1","keyword2"]
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
